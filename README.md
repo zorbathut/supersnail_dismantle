00990553b0defae05a4aa09f9e62a29d4dd47295
Ben Rog-Wilhelm <zorba-github@pavlovian.net>
2023-11-17 12:37:10 -0600

    Readme, plus some cleanup.

---
+++ README.md
@@ -0,0 +1,13 @@
+This is a tool that tells you what to dismantle in Super Snail. Good for putting together rocket parts!
+
+* Open up [blob/master/main.py](main.py)
+* Copy all the code
+* Go to your favorite Python REPL ([this one is fine](https://www.programiz.com/python-programming/online-compiler/))
+* Paste the code in
+* Look at the top of the code to configure it
+* Delete the # in front of the applicable `resource_names`
+* Fill in `current_resources` in the same pattern as is shown in the garage's Material list (it's the spherical thing on the left of the bench)
+* Fill out `required_resources` based on what you're trying to build
+* Hit the Run button
+
+It'll spit out the order to dismantle things, assuming a solution exists.
--- main.py
+++ main.py
@@ -1,6 +1,44 @@
+
+# Remove one # in front of the one that's relevant to you
+#resource_names = ["Plate", "Lens", "Radio", "Polyester", "Chip"] # Rebel Troops
+#resource_names = ["Bearing", "Gear", "Chip", "Prism", "Engine"] # Demon God
+#resource_names = ["E", "A", "R", "T", "H"] # Earth's Will
+#resource_names = ["Coolant", "Graphite", "Moderator", "Core", ""] # Dung Beetle
+#resource_names = ["Pearl", "Shell", "Conch", "Core", ""] # Goldfish
+#resource_names = ["Rune", "Crystal", "Cauldron", "Core", ""] # Surf Clam
+#resource_names = ["Futures", "Bond", "Stock", "Core", ""] # Hamster
+#resource_names = ["Fuel", "Control", "Penstock", "Core", ""] # Earthworm
+
+# Fill this in with how much you have
+current_resources = [
+    [2, 37, 11, 8, 7],  # ResourceA quantities for tiers 1-5
+    [0, 22, 30, 13, 0], # ResourceB
+    [3, 7, 6, 9, 0],  # ResourceC
+    [3, 10, 3, 13, 0],  # ResourceD
+    [0, 20, 0, 0, 0]  # ResourceE
+]
+
+# Fill this in with how much you want
+required_resources = [
+    [200, 0, 0, 0, 0],  # Required ResourceA quantities
+    [200, 0, 0, 0, 0],  # ResourceB
+    [200, 0, 0, 0, 0],  # ResourceC
+    [200, 0, 0, 0, 0],  # ResourceD
+    [100, 0, 0, 0, 0]   # ResourceE
+]
+
+
+# The rest of this is code, ignore it!
+
 import math
 import pprint
 import traceback
+
+try:
+    resource_names
+except:
+    print("You didn't uncommon a line of resource_names! Do that if you want these to be more useful.")
+    resource_names = ["Resource A", "Resource B", "Resource C", "Resource D", "Resource E"]
 
 def optimize_resources(resource_names, current_resources, required_resources):
     """
@@ -33,23 +71,6 @@ def optimize_resources(resource_names, current_resources, required_resources):
     return instructions[::-1]
 
 
-# Example input
-resource_names = ["Plate", "Lens", "Radio", "Polyester", "Chip"]
-current_resources = [
-    [2, 37, 11, 8, 7],  # ResourceA quantities for tiers 1-5
-    [0, 22, 30, 13, 0], # ResourceB
-    [3, 7, 6, 9, 0],  # ResourceC
-    [3, 10, 3, 13, 0],  # ResourceD
-    [0, 20, 0, 0, 0]  # ResourceE
-]
-required_resources = [
-    [200, 0, 0, 0, 0],  # Required ResourceA quantities
-    [200, 0, 0, 0, 0],  # ResourceB
-    [200, 0, 0, 0, 0],  # ResourceC
-    [200, 0, 0, 0, 0],  # ResourceD
-    [100, 0, 0, 0, 0]   # ResourceE
-]
-
 # Optimize resources
 try:
     instructions = optimize_resources(resource_names, current_resources, required_resources)
@@ -60,4 +81,5 @@ except Exception:
     print()
     print()
     print(traceback.format_exc())
+
 
